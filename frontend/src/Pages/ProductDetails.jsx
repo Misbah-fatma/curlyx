11a77fb83ca387b2f9d969f69dd92bef8df5bb12
Misbah Fatma <misbah7370@gmail.com>
2025-12-31 02:42:25 +0530

    message

--- frontend/src/Pages/ProductDetails.jsx
+++ frontend/src/Pages/ProductDetails.jsx
@@ -14,7 +14,7 @@ const ProductDetails = () => {
 
   useEffect(() => {
     axios
-      .get(`http://localhost:5009/api/products/${id}`)
+      .get(`https://curlyxuuuu.onrender.com/api/products/${id}`)
       .then((res) => setProduct(res.data))
       .catch((err) => console.error(err));
   }, [id]);
